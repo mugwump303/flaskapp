3b40462f428eb2fb2768e34b5afbdb228f0c92bf
mugwump303 <mugwump303@gmail.com>
2024-07-08 04:21:15 -0600

    Added gitignore and dummy changes

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
+    <h1>Dummy Change</h1>
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
