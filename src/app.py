eee7f1b94e337032209f9aa614f3f87117753187
mugwump303 <mugwump303@gmail.com>
2024-08-20 05:03:02 -0600

    schedule as get request

--- src/app.py
+++ src/app.py
@@ -24,6 +24,17 @@ def main():
     
     return render_template('index.html', runs=runs)
 
+
+@app.route("/log_scheduler/", methods=["GET"])
+def echo_input():
+    f = open("scheduler.txt", "a")
+    now = datetime.datetime.now()
+    text = f"<div>Scheduler Run: {now}</div>"
+    f.write(text)
+    f.close()
+
+    return 'Log Recevied'
+
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,9 +1,16 @@
-import datetime
+import requests
+
 # NOTE: This runs using a heroku scheduler, but, locally, it does nothing.
 
-f = open("scheduler.txt", "a")
-now = datetime.datetime.now()
-text = f"<div>Scheduler Run: {now}</div>"
+# import datetime
+# f = open("scheduler.txt", "a")
+# now = datetime.datetime.now()
+# text = f"<div>Scheduler Run: {now}</div>"
+# f.write(text)
+# f.close()
 
-f.write(text)
-f.close()
+url='https://mugwump303-flaskapp-a986c05f07c3.herokuapp.com/log_scheduler/'
+response = requests.get(url)
+print(response.status_code)
+print(response.text)
+
